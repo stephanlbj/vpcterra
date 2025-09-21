255902561138879af8f05e4d48b033d56b965c8d
stephanjames <stephanjames055@gmail.com>
2025-09-21 13:48:16 +0000

    added variables into variables root

--- variables.tf
+++ variables.tf
@@ -81,3 +81,14 @@ variable "sg_egress" {
     cidr_blocks = list(string)
   }))
 }
+
+variable "user_name" {
+  type        = string
+  description = "Nom de l'utilisateur IAM à qui on attribue les permissions"
+}
+
+variable "project_prefix" {
+  type        = string
+  description = "Préfixe utilisé pour nommer les rôles et policies pour ce projet"
+}
+
