8cfa1a4bce93b74123c86245cb8879cac47babe0
stephanjames <stephanjames055@gmail.com>
2025-09-21 13:02:51 +0000

    added a ecs module and created a cluster as well as roles

---
+++ modules/ecs/outputs.tf
@@ -0,0 +1,15 @@
+output "cluster_id" {
+  value = aws_ecs_cluster.this.id
+}
+
+output "cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
+
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "ecs_service_role_arn" {
+  value = aws_iam_role.ecs_service_role.arn
+}
---
+++ modules/ecs/variables.tf
@@ -0,0 +1,10 @@
+variable "project" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "tags" {
+  type = map(string)
+  default = {}
+}
